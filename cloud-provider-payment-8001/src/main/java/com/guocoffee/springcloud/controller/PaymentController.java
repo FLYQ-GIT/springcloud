5a1ea08d7be10d5b9e3d63fed825a33cae472840
FLYQ-GIT <1106742012@qq.com>
2020-07-02 19:54:50 +0800

    通过discoveryClient获取eureka中注册的信息

--- cloud-provider-payment-8001/src/main/java/com/guocoffee/springcloud/controller/PaymentController.java
+++ cloud-provider-payment-8001/src/main/java/com/guocoffee/springcloud/controller/PaymentController.java
@@ -5,9 +5,12 @@ import com.guocoffee.springcloud.entity.Payment;
 import com.guocoffee.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @program: springcloud
@@ -22,11 +25,18 @@ public class PaymentController {
 
     @Resource
     PaymentService paymentService;
+
     /**
      * 获取配置文件中的端口号
      */
     @Value("${server.port}")
     private String serverPort;
+
+    /**
+     * 通过服务发现获取信息
+     */
+    @Resource
+    DiscoveryClient discoveryClient;
 
     @PostMapping("/save")
     public CommonResult save(@RequestBody Payment payment){
@@ -50,5 +60,24 @@ public class PaymentController {
         }else {
             return new CommonResult(400,"失败～～～-->servicePort："+ serverPort);
         }
+    }
+
+    @GetMapping("/getClient")
+    public Object getClient(){
+        // 查询eureka中注册的所有微服务
+        List<String> services = discoveryClient.getServices();
+        log.info("所有的注册的所有微服务～～～");
+        for (String s : services) {
+            log.info(s);
+        }
+
+        // 根据微服务的具体名称获取当前微服务下所有的实例
+        log.info("CLOUD-PAYMENT-SERVICE实例～～～");
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance i:instances) {
+            log.info("注册ID："+i.getInstanceId()+"，地址："+i.getHost()+"，端口："+i.getPort()+","+i.getUri());
+        }
+
+        return discoveryClient;
     }
 }
