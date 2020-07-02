3de5a6b63df57e638c8e454fa36116834b02dc81
FLYQ-GIT <1106742012@qq.com>
2020-07-02 18:23:53 +0800

    将80订单模块以注册进eureka中

--- cloud-consumer-order-80/src/main/java/com/guocoffee/springcloud/OrderApplication80.java
+++ cloud-consumer-order-80/src/main/java/com/guocoffee/springcloud/OrderApplication80.java
@@ -2,6 +2,7 @@ package com.guocoffee.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @program: springcloud
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @create: 2020-07-02
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderApplication80 {
 
     public static void main(String[] args) {
