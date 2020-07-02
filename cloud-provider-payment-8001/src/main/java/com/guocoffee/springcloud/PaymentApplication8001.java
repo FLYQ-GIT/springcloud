cf2a33afba4057e266baf240b8549801c1c10203
FLYQ-GIT <1106742012@qq.com>
2020-07-02 18:17:42 +0800

    将8001支付模块以注册进eureka中

--- cloud-provider-payment-8001/src/main/java/com/guocoffee/springcloud/PaymentApplication8001.java
+++ cloud-provider-payment-8001/src/main/java/com/guocoffee/springcloud/PaymentApplication8001.java
@@ -2,6 +2,7 @@ package com.guocoffee.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @program: springcloud
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @create: 2020-07-02
  */
 @SpringBootApplication
+@EnableEurekaClient // 标示为Eureka客户端
 public class PaymentApplication8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentApplication8001.class,args);
