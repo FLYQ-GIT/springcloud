e9b9ac770ecbce04c3b3f33b5572b3eec4adf2b7
FLYQ-GIT <1106742012@qq.com>
2020-07-03 17:37:51 +0800

    hystrix客户端降级通用方法

--- cloud-consumer-order-80-feign-hystrix/src/main/java/com/guocoffee/springcloud/controller/PaymentController.java
+++ cloud-consumer-order-80-feign-hystrix/src/main/java/com/guocoffee/springcloud/controller/PaymentController.java
@@ -1,6 +1,7 @@
 package com.guocoffee.springcloud.controller;
 
 import com.guocoffee.springcloud.service.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
@@ -21,6 +22,7 @@ import javax.annotation.Resource;
 @RestController
 @Slf4j
 @RequestMapping("/feignHystrixPayment")
+@DefaultProperties(defaultFallback = "errorGlobalHandler")
 public class PaymentController {
 
     @Resource
@@ -40,19 +42,28 @@ public class PaymentController {
 
     /**
      * 测试超时
+     * 加上HystrixCommand注解，标示需要降级处理
+     * 若配置了fallbackMethod等明细，则按照配置的处理
+     * 若没有，则走默认配置
+     *
      * @return
      */
     @GetMapping("/timeout/{id}")
-    @HystrixCommand(fallbackMethod = "timeoutHandler",
-            commandProperties = {@HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="2000")})
+//    @HystrixCommand(fallbackMethod = "timeoutHandler",
+//            commandProperties = {@HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="2000")})
+    @HystrixCommand
     public String timeout(@PathVariable("id")long id){
-        int a = 1/0;
+//        int a = 1/0;
         String s = paymentService.paymentInfoTimeOut(id);
         return s;
     }
 
     public String timeoutHandler(long id){
         return "feignHystrixPayment客户端自己兜底，超时2秒返回";
+    }
+
+    public String errorGlobalHandler(){
+        return "全局Hystrix，统一降级方法处理～～～";
     }
 
 }
