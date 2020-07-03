1b52017d60eebe556f032bcbd196caefe56456fc
FLYQ-GIT <1106742012@qq.com>
2020-07-03 23:19:44 +0800

    gateway网关，断言基本配置

---
+++ cloud-gateway-gateway9527/src/test/java/Test.java
@@ -0,0 +1,14 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @program: springcloud
+ * @description:
+ * @author: guo-coffee
+ * @create: 2020-07-03
+ */
+public class Test {
+    public static void main(String[] args) {
+        ZonedDateTime now = ZonedDateTime.now();
+        System.out.println(now);
+    }
+}
