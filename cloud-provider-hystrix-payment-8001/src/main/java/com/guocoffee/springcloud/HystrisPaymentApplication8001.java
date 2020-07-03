098c472ffec69b0db3cdcdfc400c41a4deca78c4
FLYQ-GIT <1106742012@qq.com>
2020-07-03 16:57:43 +0800

    hystrix服务端降级

--- cloud-provider-hystrix-payment-8001/src/main/java/com/guocoffee/springcloud/HystrisPaymentApplication8001.java
+++ cloud-provider-hystrix-payment-8001/src/main/java/com/guocoffee/springcloud/HystrisPaymentApplication8001.java
@@ -2,8 +2,10 @@ package com.guocoffee.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 /**
  * @program: springcloud
@@ -13,6 +15,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient // 标示为Eureka客户端
+@EnableCircuitBreaker //
 public class HystrisPaymentApplication8001 {
     public static void main(String[] args) {
         SpringApplication.run(HystrisPaymentApplication8001.class,args);
--- cloud-provider-hystrix-payment-8001/src/main/java/com/guocoffee/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment-8001/src/main/java/com/guocoffee/springcloud/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.guocoffee.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -24,15 +26,28 @@ public class PaymentService {
 
     /**
      * 模拟超时业务
+     * 超时3秒之后调用paymentInfoTimeoutHandler方法
      * @param id
      * @return
      */
+    @HystrixCommand(fallbackMethod = "paymentInfoTimeoutHandler",
+            commandProperties = {@HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="3000")})
     public String paymentInfoTimeout(long id){
+        // 测试报错Hystrix服务降级
+//        int a = 1/0;
+
+        // 测试超时Hystrix服务降级
         try {
-            TimeUnit.SECONDS.sleep(3);
+            TimeUnit.SECONDS.sleep(5);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
         return "线程池： " + Thread.currentThread().getName() +"\t"+ "paymentInfoTimeout(),id:"+ id+ "\t" +" \"O(∩_∩) 成功返回哈哈哈";
     }
+
+    public String paymentInfoTimeoutHandler(long id){
+        return "线程池： " + Thread.currentThread().getName() +"\t"+ "paymentInfoTimeoutHandler(),id:"+ id+ "\t" +" \"超时兜底";
+    }
+
 }
