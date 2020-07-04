a068308295275de5b28962bec29c11e8cdf96453
guocoffee <1106742012@qq.com>
2020-07-04 15:13:28 +0800

    config动态刷新之手动版

--- cloud-config-client-3355/src/main/java/com/guocoffee/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3355/src/main/java/com/guocoffee/springcloud/controller/ConfigClientController.java
@@ -1,6 +1,7 @@
 package com.guocoffee.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @create: 2020-07-04
  */
 @RestController
+@RefreshScope // curl -X POST "http://localhost:3355/actuator/refresh"
 public class ConfigClientController {
 
     @Value("${config.info}")
