e3578ae8fd7ce17bf2ab4f31c52bbc259203f433
guocoffee <1106742012@qq.com>
2020-07-04 16:14:34 +0800

    config动态刷新之使用RabbitMQ定点通知

--- cloud-config-center-3344/src/main/java/com/guocoffee/springcloud/ConfigCenterApplication3344.java
+++ cloud-config-center-3344/src/main/java/com/guocoffee/springcloud/ConfigCenterApplication3344.java
@@ -11,7 +11,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  * @create: 2020-07-04
  */
 @SpringBootApplication
-@EnableConfigServer // curl -X POST "http://localhost:3344/actuator/bus-refresh" 刷新3344，通过RabbitMQ广播到3355和3366，实现自动动态刷新
+@EnableConfigServer
+// curl -X POST "http://localhost:3344/actuator/bus-refresh" 刷新3344，通过RabbitMQ广播到3355和3366，实现自动动态刷新
+// curl -X POST "http://localhost:3344/actuator/bus-refresh/config-client:3355" 定点通知(疑问：多个怎么处理)
+// config-client:3355（spring.application.name）:(端口)
 public class ConfigCenterApplication3344 {
     public static void main(String[] args) {
         SpringApplication.run(ConfigCenterApplication3344.class,args);
